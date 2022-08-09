0afe3c25d9a258e7290652271b3f06199ad92b56
LarryMays92 <LarryMays92@ymail.com>
2022-08-09 00:36:30 -0400

    show

--- src/components/shared/SneakerForm.js
+++ src/components/shared/SneakerForm.js
@@ -1,5 +1,48 @@
-const SneakerForm = (props) => {
-    return <p>Check Out The Kicks:</p>
-}
-export default SneakerForm
+import { Form, Button, Container } from 'react-bootstrap'
 
+const SneakerForm = (props) => {
+    const { sneaker, handleChange, heading, handleSubmit } = props 
+    return (
+        <Container className="justify-content-center">
+            <h3>{heading}</h3>
+            <Form onSubmit={handleSubmit}>
+        
+                <Form.Label htmlFor="brand">Brand</Form.Label>
+                <Form.Control
+                    placeholder="What brand is your sneaker?"
+                    name="brand"
+                    id="brand"
+                    value={ sneaker.brand }
+                    onChange={ handleChange }
+                    />
+                <Form.Label htmlFor="release">Release</Form.Label>
+                <Form.Control
+                    placeholder="What Year Were They Released?"
+                    type = "number"
+                    name="release"
+                    id="release"
+                    value={ sneaker.release }
+                    onChange={ handleChange }
+                    />
+                <Form.Label htmlFor="color">Colorway</Form.Label>
+                <Form.Control
+                    placeholder="Colorway"
+                    name="color"
+                    id="color"
+                    value={ sneaker.color }
+                    onChange={ handleChange }
+                    />
+                <Form.Check
+                label ="Are These Deadstock"
+                    placeholder="Are They Dead Stock"
+                    name="deadstock"
+                    defautChecked={ sneaker.deadstock }
+                    onChange={ handleChange }
+                    />
+                <Button type="submit">Submit</Button>
+            </Form>
+        </Container>
+    )
+}
+
+export default SneakerForm
--- src/components/sneakers/ShowSneaker.js
+++ src/components/sneakers/ShowSneaker.js
@@ -1,54 +1,127 @@
-import { useState, useEffect } from "react"
-import { getOneSneaker } from "../../api/sneakers"
-import { useParams, useNavigate } from "react-router-dom"
-import { Container, Card } from "react-bootstrap"
+import { useState, useEffect } from 'react'
 
-//useParams this will allow us to see our parameters
-//useNavigate this will allow us navigate to a specific page
+import { useParams, useNavigate } from 'react-router-dom'
+// useParams will allow us to see our parameters
+// useNavigate will allow us to navigate to a specific page
 
-import LoadingScreen from "../shared/LoadingScreen"
+import { Container, Card, Button } from 'react-bootstrap'
+
+import LoadingScreen from '../shared/LoadingScreen'
+import { getOneSneaker, updateSneaker, removeSneaker } from '../../api/sneakers'
 import messages from '../shared/AutoDismissAlert/messages'
+import EditSneakerModal from './EditSneakerModal'
 
-//We need to get the pet's id from the parameters
-//Then we need to make a request to the api
-//Then we need to display the results in this component
+// We need to get the sneaker's id from the parameters
+// Then we need to make a request to the api
+// Then we need to display the results in this component
+
 const ShowSneaker = (props) => {
     const [sneaker, setSneaker] = useState(null)
+    const [editModalShow, setEditModalShow] = useState(false)
+    const [updated, setUpdated] = useState(false)
+
     const { id } = useParams()
-    // ^^deconstructuring to get the id value from our route parameters
     const navigate = useNavigate()
-    const { msgAlert } = props
+    // useNavigate returns a function
+    // we can call that function to redirect the user wherever we want to
+
+    const { user, msgAlert } = props
+    console.log('user in props', user)
+    console.log('the sneaker in showSneaker', sneaker)
+    // destructuring to get the id value from our route parameters
 
     useEffect(() => {
         getOneSneaker(id)
             .then(res => setSneaker(res.data.sneaker))
-            .catch(err => {
-               msgAlert({heading: 'Error Getting Sneakers',
-                message: messages.getSneakersFailure,
-                   variant: 'danger',
-               })
+            .catch(err => {                   
+                msgAlert({
+                    heading: 'Error getting sneaker',
+                    message: messages.getSneakersFailure,
+                    variant: 'danger'
+                })
                 navigate('/')
-                //navigate back to the homepage if there's an error
+                //navigate back to the home page if there's an error fetching
             })
-    }, [])
-    
-    if (!sneaker) {
-        return<LoadingScreen/>
-    }
-    return (
-        <Container className="fluid">
-            <Card>
-                <Card.Header>{sneaker.fullTitle}</Card.Header>
-                <Card.Body>
-                    <Card.Text>
-                        <div><small>Release:{sneaker.release}</small></div>
-                        <div><small>Colorway:{sneaker.type}</small></div>
-                        <div><small>Deadstock:{sneaker.deadstock ? 'yes' : 'no'}</small></div>
-                    </Card.Text>
-                </Card.Body>
-            </Card>
-        </Container>
-    )
-}
-export default ShowSneaker
+    }, [updated])
 
+    // here we'll declare a function that runs which will remove the sneaker
+    // this function's promise chain should send a message, and then go somewhere
+    const removeTheSneaker = () => {
+        removeSneaker(user, sneaker.id)
+            // on success send a success message
+            .then(() => {
+                msgAlert({
+                    heading: 'Success',
+                    message: messages.removeSneakerSuccess,
+                    variant: 'success'
+                })
+            })
+            // then navigate to index
+            .then(() => {navigate('/')})
+            // on failure send a failure message
+            .catch(err => {                   
+                msgAlert({
+                    heading: 'Error removing sneaker',
+                    message: messages.removeSneakerFailure,
+                    variant: 'danger'
+                })
+            })
+    }
+
+    if (!sneaker) {
+        return <LoadingScreen />
+    }
+
+    return (
+        <>
+            <Container className="fluid">
+                <Card>
+                    <Card.Header>{ sneaker.brand }</Card.Header>
+                    <Card.Body>
+                        <Card.Text>
+                            <div><small>Brand: { sneaker.colorway }</small></div>
+                            <div><small>Release: { sneaker.release }</small></div>
+                            <div><small>
+                                Deadstock: { sneaker.deadstock ? 'yes' : 'no'}
+                            </small></div>
+                        </Card.Text>
+                    </Card.Body>
+                    <Card.Footer>
+                        {
+                            sneaker.owner && user && sneaker.owner._id === user._id 
+                            ?
+                            <>
+                                <Button onClick={() => setEditModalShow(true)} 
+                                    className="m-2" 
+                                    variant="warning"
+                                >
+                                    Edit Sneaker
+                                </Button>
+                                <Button onClick={() => removeTheSneaker()}
+                                    className="m-2"
+                                    variant="danger"
+                                >
+                            {sneaker.name} Were Sold Or Donated
+                                </Button>
+                            </>
+                            :
+                            null
+                        }
+                    </Card.Footer>
+                </Card>
+            </Container>
+            <EditSneakerModal 
+                user={user}
+                sneaker={sneaker} 
+                show={editModalShow} 
+                updateSneaker={updateSneaker}
+                msgAlert={msgAlert}
+                triggerRefresh={() => setUpdated(prev => !prev)}
+                handleClose={() => setEditModalShow(false)} 
+            />
+        </>
+    )
+
+}
+
+export default ShowSneaker
