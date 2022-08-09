fbd3d163fe99cdd0f86ed9cf485a27c7e2cc0d00
LarryMays92 <LarryMays92@ymail.com>
2022-08-09 00:54:23 -0400

    home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,11 @@
 import SneakersIndex from "./sneakers/SneakersIndex"
+
 const Home = (props) => {
     // const { msgAlert, user } = props
     console.log('props in home', props)
+
     const { msgAlert } = props
+    
     return (
         <>
             <h2>Welcome To The Your Closet</h2>
