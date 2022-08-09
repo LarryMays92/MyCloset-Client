e3b6487ba68b92e00d5063952621dca1aeab5e52
LarryMays92 <LarryMays92@ymail.com>
2022-08-09 00:03:36 -0400

    sneakers api

--- src/api/sneakers.js
+++ src/api/sneakers.js
@@ -6,3 +6,46 @@ export const getAllSneakers = () => {
 export const getOneSneaker = (id) => {
     return axios(`${apiUrl}/sneakers/${id}`)
 }
+
+export const createSneaker = (user, newSneaker) => {
+    // console.log('createShoe in api was hit')
+    // console.log('this is user', user)
+    // console.log('this is newShoe', newShoe)
+    return axios({
+		url: apiUrl + '/sneakers',
+		method: 'POST',
+		headers: {
+			Authorization: `Token token=${user.token}`,
+		},
+		data: { sneaker:  newSneaker },
+	})
+}
+
+// UPDATE
+export const updateSneaker = (user, updatedSneaker) => {
+    // in our createsneakerform, we're building an object
+    // when we pass that object into the api createSneaker function,
+    // it's going to look like the sneaker in our database
+    // we're going to refer to this as newSneaker
+    // console.log('this is user', user)
+    console.log('this is updatedSneaker', updatedSneaker)
+	return axios({
+		url: `${apiUrl}/sneakers/${updatedSneaker.id}`,
+		method: 'PATCH',
+		headers: {
+			Authorization: `Token token=${user.token}`,
+		},
+		data: { shoe: updatedSneaker }
+	})
+}  
+
+// DELETE
+export const removeSneaker = (user, sneakerId) => {
+    return axios({
+        url: `${apiUrl}/sneakers/${sneakerId}`,
+        method: 'DELETE',
+        headers: {
+            Authorization: `Token token=${user.token}`,
+        }
+    })
+} 
